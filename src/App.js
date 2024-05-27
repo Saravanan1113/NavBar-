c4637358429f6516f4d81a7793e9d1317f22bcc6
Saravanan1113 <saravanarajendran1998@gmail.com>
2024-05-27 10:55:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./components/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import PageOne from "./Pages/PageOne";
+import PageTwo from "./Pages/PageTwo";
+import PageThree from "./Pages/PageThree";
+import PageCTA from "./Pages/PageCTA";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/page-one" element={<PageOne />} />
+          <Route path="/page-two" element={<PageTwo />} />
+          <Route path="/page-three" element={<PageThree />} />
+
+          <Route path="/page-cta" element={<PageCTA />} />
+          {/* <Home />
+          </Route>
+          <Route path="/page-one">
+            <PageOne />
+          </Route>
+          <Route path="/page-two">
+            <PageTwo />
+          </Route>
+          <Route path="/page-three">
+            <PageThree />
+          </Route>
+          <Route path="/page-cta">
+            <PageCTA />
+          </Route> */}
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
